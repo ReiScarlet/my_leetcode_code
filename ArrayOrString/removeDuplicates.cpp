755c541162f667a6e9b58fc9d0ae865d2188c655
ReiScarlet <2285345090@qq.com>
2024-05-06 18:09:09 +0800

    add removeDuplicate template for any k

--- ArrayOrString/removeDuplicates.cpp
+++ ArrayOrString/removeDuplicates.cpp
@@ -17,4 +17,50 @@ public:
     }
     return lenght + 1;
   }
+
+  // 题号: 80
+  int removeDuplicates_II(vector<int>& nums)
+  {
+    // const int& n = nums.size();
+    // if (n <= 2) {
+    //   return n;
+    // }
+
+    // int slow = 2, fast = 2;
+    // while (fast < n) {
+    //   // std::printf("nums[%d-2]=%d nums[%d]=%d", slow, nums[slow - 2], fast,
+    //   // nums[fast]);
+    //   if (nums[slow - 2] != nums[fast]) {
+    //     nums[slow] = nums[fast];
+    //     ++slow;
+    //   }
+    //   ++fast;
+    //   // std::printf(" slow[%d] fast[%d]\n", slow, fast);
+    // }
+
+    // return slow;
+
+    return removeDulicatesTMP(nums, 2);
+  }
+
+  int removeDulicatesTMP(vector<int>& nums, int k)
+  {
+    int len = 0;
+    for (const int& num : nums) {
+      if (len < k || nums[len - k] != num) {
+        nums[len] = num;
+        len++;
+      }
+    }
+    return len;
+  }
 };
+
+int
+main()
+{
+  auto pSoulution = std::make_shared<Solution>();
+  std::vector<int> nums{ 1, 1, 1, 2, 2, 3, 3 };
+  pSoulution->removeDuplicates_II(nums);
+  return 0;
+}
