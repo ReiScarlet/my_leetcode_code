3339c8c88383da5d2fb6bc90669bb52ef0005cf9
ReiScarlet <2285345090@qq.com>
2024-06-11 14:58:49 +0800

    problem.274 add note

--- ArrayOrString/274-hIndex.cpp
+++ ArrayOrString/274-hIndex.cpp
@@ -2,6 +2,12 @@
 
 class Solution {
  public:
+  // 对citation进行排序后，对于每个元素，都有大于等于citation[i]的值有citations.size()-i个
+  // 由于h指数是找出，大于等于h的值至少有h个，则h值应为citations[i]与citations.size()-i之间的最小值
+  // 再有题目直接对所有找出的h指数筛选最大值即可
+  //
+  // 举个例子，如果我有100个1组成的序列，那么h指数的值为1，因为大于等于1的值至少有1个
+  // 反之，有1个100的序列，其h指数也为1
   int hIndex(std::vector<int>& citations) {
     std::sort(citations.begin(), citations.end());
     int ans = 0;
