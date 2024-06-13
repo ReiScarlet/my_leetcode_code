6f4be52a2f1084eea52040f348a7d48bceed401e
ReiScarlet <2285345090@qq.com>
2024-06-13 10:26:04 +0800

    problem.392

---
+++ DoublePointer/392-isSubsequence.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+class Solution {
+ public:
+  bool isSubsequence(std::string s, std::string t) {
+    const int& n = s.size();
+    int left = 0, right = t.size();
+    for (int pos = 0; pos < n; pos++) {
+    }
+  }
+};
