d8a5a2008550e4f3a521b0b9a9b587d99f225aff
ReiScarlet <2285345090@qq.com>
2024-06-13 10:47:35 +0800

    problem.392 add note

--- DoublePointer/392-isSubsequence.cpp
+++ DoublePointer/392-isSubsequence.cpp
@@ -2,6 +2,8 @@
 
 class Solution {
  public:
+  // 只需要分别按顺序查找s中字符是否在t中按同样的顺序即可
+  // 注意：s为空时是任何串的子串
   bool isSubsequence(std::string s, std::string t) {
     if (s == "") return true;
     const int& n1 = s.size();
