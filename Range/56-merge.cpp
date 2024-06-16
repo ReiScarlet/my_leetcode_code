138a4e96abcb41e27343f0651897f8660330eade
ReiScarlet <2285345090@qq.com>
2024-06-16 10:32:04 +0800

    problem.56

---
+++ Range/56-merge.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::vector<int>> merge(
+      std::vector<std::vector<int>>& intervals) {
+    if (intervals.size() == 0) {
+      return {};
+    }
+    std::sort(intervals.begin(), intervals.end());
+    std::vector<std::vector<int>> merged;
+    const int n = intervals.size();
+    for (int i = 0; i < n; ++i) {
+      int L = intervals[i][0], R = intervals[i][1];
+      if (!merged.size() || merged.back()[1] < L) {  // new array
+        merged.push_back({L, R});
+      } else { // interval
+        merged.back()[1] = std::max(merged.back()[1], R);
+      }
+    }
+    return merged;
+  }
+};
