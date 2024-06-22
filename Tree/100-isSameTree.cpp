14341dabd6f68ae31d6b6e19720a579bd32bd5e4
ReiScarlet <2285345090@qq.com>
2024-06-22 11:30:30 +0800

    format the file

--- Tree/100-isSameTree.cpp
+++ Tree/100-isSameTree.cpp
@@ -1,23 +1,21 @@
-struct TreeNode
-{
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode() : val(0), left(nullptr), right(nullptr) {}
-	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-	TreeNode(int x, TreeNode *left, TreeNode *right)
-		: val(x), left(left), right(right) {}
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
 };
 
-class Solution
-{
+class Solution {
 public:
-	bool isSameTree(TreeNode *p, TreeNode *q)
-	{
-		if ((p && !q) || (!p && q))
-			return false;
-		if (!p && !q)
-			return true;
-		return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
-	}
+    bool isSameTree(TreeNode *p, TreeNode *q) {
+        if ((p && !q) || (!p && q))
+            return false;
+        if (!p && !q)
+            return true;
+        return p->val == q->val && isSameTree(p->left, q->left) &&
+               isSameTree(p->right, q->right);
+    }
 };
